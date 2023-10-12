4d7b19e86ae5a222eadff62428617f22a69aa2b8
Mitesh singh <102028317+Mitesh957@users.noreply.github.com>
2023-10-12 07:32:51 +0530

    Add files via upload

---
+++ Hack Mania/firebase-config.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaphyiJbOypu_RpyjwQgs56MokaS6hjKg",
+  authDomain: "taxease-63b84.firebaseapp.com",
+  databaseURL: "https://taxease-63b84-default-rtdb.firebaseio.com",
+  projectId: "taxease-63b84",
+  storageBucket: "taxease-63b84.appspot.com",
+  messagingSenderId: "728547019274",
+  appId: "1:728547019274:web:2815a01d7d8575a442adf3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
